b4f0323ae930c719407399feed0ba318d5260f3c
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2023-03-11 00:51:54 +0300

    clear code

--- src/pages/components/modals-windows/index.js
+++ src/pages/components/modals-windows/index.js
@@ -1,4 +1,1 @@
 export { ModalWindow } from './modal-window';
-export { ModalSuccessRecovery } from './modal-success-recovery';
-export { ModalSuccessForgot } from './modal-success-forgot';
-export { ModalErrorRecovery } from './modal-error-recovery';
