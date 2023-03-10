c6a854b56ec242ffe953dc6d23f25aa096f4289d
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2023-03-10 16:52:24 +0300

    fix color input and made a constant for the regular expression

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,4 +1,5 @@
 import { object, string, lazy, ref } from 'yup';
+import { RegExp } from './regexp';
 
 export const authenticationSchema = object({
   identifier: string().required('Поле не может быть пустым'),
@@ -8,13 +9,13 @@ export const authenticationSchema = object({
 export const registerStepOneSchema = object({
   username: string()
     .required('Поле не может быть пустым')
-    .matches(/(?=.*[A-Za-z]).{1,}/, 'латинский алфавит')
-    .matches(/(?=.*\d).{1,}/, 'цифры'),
+    .matches(RegExp.leastOneLatinLetter, 'латинский алфавит')
+    .matches(RegExp.leastOneDigit, 'цифры'),
   password: string()
     .required('Поле не может быть пустым')
-    .matches(/[A-Za-z0-9]{8,}/, 'не менее 8 символов')
-    .matches(/(?=.*[A-Z])/, 'с заглавной буквой')
-    .matches(/(?=.*[0-9])/, 'цифрой'),
+    .matches(RegExp.leastEightCharacters, 'не менее 8 символов')
+    .matches(RegExp.capitalized, 'с заглавной буквой')
+    .matches(RegExp.numbers, 'цифрой'),
 });
 
 export const registerStepTwoSchema = object({
@@ -23,35 +24,20 @@ export const registerStepTwoSchema = object({
 });
 
 export const registerStepThreeSchema = object({
-  phone: string()
-    .required('Поле не может быть пустым')
-    .matches(
-      /^\+?375((\s\(33\)\s\d{3}-\d{2}-\d{2})|(\s\(29\)\s\d{3}-\d{2}-\d{2})|(\s\(44\)\s\d{3}-\d{2}-\d{2})|(\s\(25\)\s\d{3}-\d{2}-\d{2}))\s*$/,
-      'В формате +375 (xx) xxx-xx-xx'
-    ),
-  email: string()
-    .required('Поле не может быть пустым')
-    .matches(
-      /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i,
-      'Введите корректный e-mail'
-    ),
+  phone: string().required('Поле не может быть пустым').matches(RegExp.phoneNumber, 'В формате +375 (xx) xxx-xx-xx'),
+  email: string().required('Поле не может быть пустым').matches(RegExp.email, 'Введите корректный e-mail'),
 });
 
 export const forgotPasswordSchema = object({
-  email: string()
-    .required('Поле не может быть пустым')
-    .matches(
-      /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i,
-      'Введите корректный e-mail'
-    ),
+  email: string().required('Поле не может быть пустым').matches(RegExp.email, 'Введите корректный e-mail'),
 });
 
 export const recovetyPasswordSchema = object({
   password: string()
     .required('Поле не может быть пустым')
-    .matches(/[0-9a-zA-Z]{8,}/, 'не менее 8 символов')
-    .matches(/(?=.*[A-Z])/, 'с заглавной буквой')
-    .matches(/(?=.*[0-9])/, 'цифрой'),
+    .matches(RegExp.leastEightCharacters, 'не менее 8 символов')
+    .matches(RegExp.capitalized, 'с заглавной буквой')
+    .matches(RegExp.numbers, 'цифрой'),
   passwordConfirmation: lazy((value) =>
     string().when('passwordConfirmation', (any, schema) =>
       value === ''
